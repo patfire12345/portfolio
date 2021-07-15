15dbbd2967cd5516bce9e4ccb00d29ea6e5b53f2
unknown <patpat12345@hotmail.com>
2021-07-15 17:51:48 -0400

    Slight bug with the revolver images

--- src/components/JavaScript Pages/Revolver.js
+++ src/components/JavaScript Pages/Revolver.js
@@ -6,32 +6,40 @@ function Revolver() {
     // represents the project index
     const [counter, setCounter] = useState(0);
     return (
-        <div style={{textAlign: "center"}}>
-            <button 
-            style={{position: "absolute", right: "1000px", top: "1300px"}}
-            onClick={() => {
-                if (counter <= 0) {
-                    setCounter(texts.length-1);
-                }
-                else {
-                    setCounter(counter - 1)
-                }
-            }}>
-                <Left/>
-            </button>
-            <img src={texts[counter].img} style={{width:"25%", height: "25%", margin: "10px"}}/>
-            <button 
-            style={{position: "absolute", right: "200px", top: "1300px"}}
-            onClick={() => {
-                if (counter >= texts.length-1) {
-                    setCounter(0);
-                }
-                else {
-                    setCounter(counter + 1)
-                }
-            }}>
-                <Right/>
-            </button>
+        <div>
+            <div style={{textAlign: "center"}}>
+                <button 
+                style={{position: "absolute", right: "1000px", top: "1300px"}}
+                onClick={() => {
+                    if (counter <= 0) {
+                        setCounter(texts.length-1);
+                    }
+                    else {
+                        setCounter(counter - 1)
+                    }
+                }}>
+                    <Left/>
+                </button>
+                <a href={texts[counter].link}>
+                    <img src={texts[counter].img} style={{width:"25%", height: "25%", margin: "10px"}}/>
+                </a>
+                <button 
+                style={{position: "absolute", right: "200px", top: "1300px"}}
+                onClick={() => {
+                    if (counter >= texts.length-1) {
+                        setCounter(0);
+                    }
+                    else {
+                        setCounter(counter + 1)
+                    }
+                }}>
+                    <Right/>
+                </button>
+                <b/>
+            </div>
+            <div>
+                {texts[counter].body}
+            </div>
         </div>
     )
 }
